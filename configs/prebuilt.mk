8acaeba37cf6e5bcf3dbe5b08aec7fe1c81fbb64
贺小帅 <heshuai1992@gmail.com>
2018-06-19 20:02:09 +0800

    Update the config for version Flyme 7.8.6.19
    Change-Id: I61ff26560345f143603959b3cbf839062d00abfe

--- configs/prebuilt.mk
+++ configs/prebuilt.mk
@@ -15,31 +15,10 @@ BOARD_PREBUILT += \
     framework/flyme-res/flyme-res.apk \
     framework/flyme-framework.jar \
     framework/flyme-telephony-common.jar \
-    lib/libblasV8.so \
-    lib/libdlna_jni.so \
-    lib/libDominantColors.so \
-    lib/libeffects_filters.so \
-    lib/libeffects_mosaic.so \
-    lib/libeglbitmap.so \
-    lib/libexif_gallery.so \
-    lib/libextmedia_jni.so \
-    lib/libfilterUtils.so \
     lib/libglrenderer.so \
-    lib/libimage_codec.so \
-    lib/libimage_dehazing.so \
-    lib/libimageproc.so \
-    lib/libjni_gauss_blur.so \
-    lib/libjni_glrenderer.so \
     lib/libjni_systemuiex.so \
     lib/libjni_systemui.so \
-    lib/libmcode_image.so \
-    lib/libnative_blur.so \
     lib/libnicipher.so \
-    lib/libphoto_process.so \
-    lib/librender_engine.so \
-    lib/libRSSupport.so \
-    lib/libskia_hw_interface.so \
-    lib/libsurfacetexture_bitmap.so \
     lib/libtaglib.so \
     lib/libvlife_media.so \
     lib/libvlife_openglutil.so \
--- configs/prebuilt_64.mk
+++ configs/prebuilt_64.mk
@@ -1,27 +1,10 @@
 # prebuilt_64.mk
 
 BOARD_PREBUILT += \
-    lib64/libdlna_jni.so \
-    lib64/libDominantColors.so \
-    lib64/libeffects_filters.so \
-    lib64/libeffects_mosaic.so \
-    lib64/libeglbitmap.so \
-    lib64/libexif_gallery.so \
-    lib64/libextmedia_jni.so \
-    lib64/libfilterUtils.so \
     lib64/libglrenderer.so \
-    lib64/libimage_codec.so \
-    lib64/libimage_dehazing.so \
-    lib64/libimageproc.so \
-    lib64/libjni_glrenderer.so \
     lib64/libjni_systemuiex.so \
     lib64/libjni_systemui.so \
-    lib64/libmcode_image.so \
-    lib64/libnative_blur.so \
     lib64/libnicipher.so \
-    lib64/libphoto_process.so \
-    lib64/librender_engine.so \
-    lib64/libskia_hw_interface.so \
     lib64/libtaglib.so \
     lib64/libvlife_media.so \
     lib64/libvlife_openglutil.so \
